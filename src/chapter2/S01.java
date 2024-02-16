460e93f46074b76927330f9090c16e141440d5c9
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-16 15:13:25 +0300

    initial commit

--- src/chapter2/S01.java
+++ src/chapter2/S01.java
@@ -1,7 +1,15 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class S01 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print(" Lütfen Santigrat Değerini Giriniz :");
+        double santigrat = scan.nextDouble();
+
+        double fahrenhayt = (9.0 / 5) * santigrat + 32;
+        System.out.println(santigrat + " Santigrat " +fahrenhayt + " Fahrenhayttır ");
     }
 }
---
+++ src/chapter3/S01.java
@@ -0,0 +1,7 @@
+package chapter3;
+
+public class S01 {
+    public static void main(String[] args) {
+
+    }
+}
