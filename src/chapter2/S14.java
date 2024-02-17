1ad276b5a3890046194f660a8bfc311bf2df2b27
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-17 20:05:32 +0300

    initial commit

--- src/chapter2/S14.java
+++ src/chapter2/S14.java
@@ -1,7 +1,20 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class S14 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen ağırlığınızı pound cinsinden giriniz :");
+        double agırlık = scan.nextDouble();
+        System.out.print(" Lütfen boyunuzu inch cinsinden giriniz :");
+        double boyUzunlugu = scan.nextDouble();
+
+        double sonuc1 = agırlık * 0.45359237; // Bizim girdiğimiz pound değerinin kg ye dönüşümü
+        double sonuc2 = boyUzunlugu * 0.0254; // Bizim girdiğimiz inch değerinin metreye dönüşümü
+        double vucutKitleEndeksi = sonuc1 / (sonuc2 * sonuc2);
+
+        System.out.println(" Vücut Kitle Endeksi :" +vucutKitleEndeksi);
 
     }
 }
--- src/chapter2/S18.java
+++ src/chapter2/S18.java
@@ -3,5 +3,18 @@ package chapter2;
 public class S18 {
     public static void main(String[] args) {
 
+        System.out.println(" a\t    b\t  pow(a, b)");
+
+        int sonuc1 = (int) Math.pow(1,2);
+        System.out.println(" 1\t    2\t \t " + sonuc1);
+        int sonuc2 = (int) Math.pow(2,3);
+        System.out.println(" 1\t    2\t \t " + sonuc2);
+        int sonuc3 = (int) Math.pow(3,4);
+        System.out.println(" 1\t    2\t \t " + sonuc3);
+        int sonuc4= (int) Math.pow(4,5);
+        System.out.println(" 1\t    2\t \t " + sonuc4);
+        int sonuc5= (int) Math.pow(5,6);
+        System.out.println(" 1\t    2\t \t " + sonuc5);
+
     }
 }
