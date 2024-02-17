c314a423a44b564b48c1a13801c5be993b46caed
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-17 20:07:02 +0300

    initial commit

--- src/chapter2/S15.java
+++ src/chapter2/S15.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 
 public class S15 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print(" Lütfen kenar uzunluğunu (s) giriniz :");
-        double kenarUzunlugu = scan.nextDouble();
 
-        System.out.println(3* Math.sqrt(3));
-        double alan =  (3 * Math.sqrt(3) / 2) * (Math.pow(kenarUzunlugu , 2));
-        System.out.println(" Altıgenin Alanı :" +alan);
     }
 }
--- src/chapter2/S16.java
+++ src/chapter2/S16.java
@@ -1,7 +1,15 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class S16 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen kenar uzunluğunu (s) giriniz :");
+        double kenarUzunlugu = scan.nextDouble();
 
+        System.out.println(3* Math.sqrt(3));
+        double alan =  (3 * Math.sqrt(3) / 2) * (Math.pow(kenarUzunlugu , 2));
+        System.out.println(" Altıgenin Alanı :" +alan);
     }
 }
