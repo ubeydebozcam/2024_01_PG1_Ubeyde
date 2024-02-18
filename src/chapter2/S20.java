1eb88954a15c14340c817067e0dba00f97fa2c47
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-18 15:45:44 +0300

    initial commit

--- src/chapter2/S20.java
+++ src/chapter2/S20.java
@@ -1,7 +1,17 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class S20 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen bakiyeyi giriniz :");
+        double bakiye = scan.nextDouble();
+        System.out.print(" Lütfen yıllık faiz oranını giriniz :");
+        double yillikFaizOrani = scan.nextDouble();
 
+        double faizGetirisi = bakiye * (yillikFaizOrani / 1200);
+
+        System.out.println(" Faiz getirisi :" +faizGetirisi);
     }
 }
