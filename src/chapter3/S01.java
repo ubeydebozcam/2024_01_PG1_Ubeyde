8a7e7f7e816e98e401c563c72e4846b183860bc5
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-18 16:27:35 +0300

    initial commit

--- src/chapter3/S01.java
+++
@@ -1,7 +0,0 @@
-package chapter3;
-
-public class S01 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/chapter3/S02.java
+++
@@ -1,7 +0,0 @@
-package chapter3;
-
-public class S02 {
-    public static void main(String[] args) {
-
-    }
-}
