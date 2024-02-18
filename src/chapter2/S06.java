bfc585f9185aefb7061ee712116d6025feab54bd
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-18 23:41:32 +0300

    initial commit

--- src/chapter2/S06.java
+++ src/chapter2/S06.java
@@ -1,7 +1,23 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class S06 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen 0 ile 1000 arasında bir sayı giriniz :");
+        int sayi = scan.nextInt();
 
+        int basamak1 = sayi % 10;
+        System.out.println(basamak1);
+        int basamak1Silme = sayi / 10;
+        System.out.println(basamak1Silme);
+        int basamak2 =  (basamak1Silme % 10);
+        System.out.println(basamak2);
+        int basamak2Silme =  (basamak1Silme / 10);
+        System.out.println(basamak2Silme);
+
+        int sonuc = (basamak1 + basamak2 + basamak2Silme);
+        System.out.println(" Girilen sayının rakamları toplamı :" +sonuc );
     }
 }
