dd9d67f074fecf6bd26432253a4f1618da724cc4
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-18 22:51:30 +0300

    initial commit

--- src/chapter2/S11.java
+++ src/chapter2/S11.java
@@ -1,7 +1,35 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class S11 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int suAnkiNufus = 312032486;
+        int dakikadaDoğum = 8;
+        int dakikadaÖlüm = 4;
+        int dakikadaGelenGöçmen = 1;
+        System.out.println(" Dakikada Nüfus Artışı : " + (dakikadaDoğum + dakikadaGelenGöçmen
+                - dakikadaÖlüm) + " Kişidir");
+        int saatteDoğum = dakikadaDoğum * 60;
+        int saatteÖlüm = dakikadaÖlüm * 60;
+        int saatteGelenGöcmen = dakikadaGelenGöçmen * 60;
+        System.out.println(" Saatte Nüfus Artışı : " + (saatteDoğum + saatteGelenGöcmen - saatteÖlüm) + " Kişidir");
+        int gündeDoğum = saatteDoğum * 24;
+        int gündeÖlüm = saatteÖlüm * 24;
+        int gündeGelenGöçmen = saatteGelenGöcmen * 24;
+        System.out.println(" Günde Nüfus Artışı : " + (gündeDoğum + gündeGelenGöçmen - gündeÖlüm) + " Kişidir");
+        int yıldaDoğum = gündeDoğum * 365;
+        int yıldaÖlüm = gündeÖlüm * 365;
+        int yıldaGelenGöçmen = gündeGelenGöçmen * 365;
+        System.out.println(" 1 Yılda Nüfus Artışı : " + (yıldaDoğum + yıldaGelenGöçmen - yıldaÖlüm) + " Kişidir");
+        System.out.print(" Lütfen yıl sayısını giriniz :");
+        int yilSayisi = scan.nextInt();
+
+        int yillikArtis = 365 * 24 * 60 * 60 / 7 - 365 * 24 * 60 * 60 / 13 + 365 * 24 * 60 * 60 / 45;
+        int nufus = suAnkiNufus + yilSayisi * yillikArtis;
+
+        System.out.println(yilSayisi + " yıl sonra tahmini nüfus: " + nufus);
 
     }
 }
--- src/chapter2/S22.java
+++ src/chapter2/S22.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class S22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
     }
 }
