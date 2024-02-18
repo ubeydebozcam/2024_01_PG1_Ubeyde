81943c7469bdf6fbcaa5c9763410075b4ad90d87
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-18 15:37:37 +0300

    initial commit

--- src/chapter2/S19.java
+++ src/chapter2/S19.java
@@ -1,7 +1,30 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class S19 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen x1 noktasını giriniz :");
+        double x1 = scan.nextDouble();
+        System.out.print(" Lütfen y1 noktasını giriniz :");
+        double y1 = scan.nextDouble();
+        System.out.print(" Lütfen x2 noktasını giriniz :");
+        double x2 = scan.nextDouble();
+        System.out.print(" Lütfen y2 noktasını giriniz :");
+        double y2 = scan.nextDouble();
+        System.out.print(" Lütfen x3 noktasını giriniz :");
+        double x3 = scan.nextDouble();
+        System.out.print(" Lütfen y3 noktasını giriniz :");
+        double y3 = scan.nextDouble();
+
+        double side1 = Math.sqrt(Math.pow(x2 - x1 , 2) + Math.pow(y2 - y1 , 2));
+        double side2 = Math.sqrt(Math.pow(x3 - x2 , 2) + Math.pow(y3 - y2 , 2));
+        double side3 = Math.sqrt(Math.pow(x3 - x1 , 2) + Math.pow(y3 - y1 , 2));
+        double yarıCevre = (side1 + side2 + side3) / 2;
+        double alan = Math.sqrt(yarıCevre * (yarıCevre - side1) * (yarıCevre - side2) * (yarıCevre - side3));
+
+        System.out.println(" Üçgenin Alanı :" +alan);
 
     }
 }
