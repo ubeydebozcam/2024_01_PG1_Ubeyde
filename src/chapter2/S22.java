e8e74b8a087dc5efe9637b44e58b66e172814756
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-19 19:39:09 +0300

    initial commit

--- src/chapter2/S22.java
+++ src/chapter2/S22.java
@@ -6,8 +6,9 @@ public class S22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print(" Lütfen para miktarını giriniz :");
-        double miktar = scan.nextDouble();
-        int kalanMiktar = (int) (miktar * 100);
+        int miktar = scan.nextInt();
+        int kalanMiktar = miktar;
+
         int dolarSayısı = kalanMiktar / 100;
         kalanMiktar %= 100;
         int yarım = kalanMiktar / 50;
@@ -20,13 +21,13 @@ public class S22 {
          kalanMiktar %= 5;
          int birKuruslar = kalanMiktar;
 
-        System.out.println(" Para miktarı " + miktar + " dolardır :");
-        System.out.println("              " + dolarSayısı + " Tane Dolar ");
+        System.out.println(" Para miktarı " + miktar / 100.0 + " dolardır :");
+        System.out.println("              " + dolarSayısı + " Tane dolar ");
         System.out.println("              " + yarım + " Tane elli kuruş ");
         System.out.println("              " + ceyrekler + " Tane çeyrek ");
-        System.out.println("              " + onKurusluklar + " Tane on KUruş ");
+        System.out.println("              " + onKurusluklar + " Tane on kuruş ");
         System.out.println("              " + besKurusluklar + " Tane beş kuruş ");
-        System.out.println("              " + birKuruslar + " Tane bir Kuruş ");
+        System.out.println("              " + birKuruslar + " Tane bir kuruş ");
 
     }
 }
