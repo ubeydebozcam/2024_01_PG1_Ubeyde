bd33342152165e59663c2fa1a542dafc459eafb9
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-02-20 14:38:21 +0300

    initial commit

--- src/chapter3/S01.java
+++ src/chapter3/S01.java
@@ -13,19 +13,21 @@ public class S01 {
         double c = scan.nextDouble();
 
         double diskriminant = Math.pow(b , 2) - (4 * a * c);
-        if (diskriminant > 0){
+        if (diskriminant > 0)
+            {
             System.out.println(" Denklemin iki kökü vardır ");
             double kok1 = (-b + Math.pow(diskriminant, 0.5)) / (2 * a);
             double kok2 = (-b - Math.pow(diskriminant, 0.5)) / (2 * a);
             System.out.println(" Denlemin 1. kökü :" +kok1 + " Denklemin ikinci kökü :" +kok2);
-
-        } else if (diskriminant ==0) {
+            }
+         else if (diskriminant ==0)
+            {
             System.out.println(" Denklemin bir kökü vardır ");
             double kok1 = (-b - Math.pow(diskriminant, 0.5)) / (2 * a);
             System.out.println(" Denlemin kökü :" +kok1);
-
-        }else{
+            }
+        else
             System.out.println(" Denklemin kökü yoktur ");
-        }
+
     }
 }
