3dad7adfcba348029f6aa32f9a9560be5f7f2636
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-04-08 21:45:17 +0300

    initial commit

---
+++ src/chapter3/S20.java
@@ -0,0 +1,17 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class S20 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen -58 Fahrenhayt ve 41 Fahrenhayt arasında bir sıcaklık giriniz :");
+        double sıcaklık = scan.nextDouble();
+        System.out.print(" Lütfen rüzgar hız değerini saatte en az 2 mil olacak şekilde giriniz :");
+        double ruzgarHizi = scan.nextDouble();
+
+
+
+
+    }
+}
