2cd1f15a39bfd7672a4a44d92733eca243e86411
ubeydebozcam <157588417+ubeydebozcam@users.noreply.github.com>
2024-04-24 18:50:17 +0300

    initial commit

---
+++ src/chapter4/S01.java
@@ -0,0 +1,19 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class S01 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen beşgenin merkezinden köşesine kadar olan uzunluğu  giriniz :");
+        double uzunluk = scan.nextDouble();
+
+        double s = (2 * uzunluk) * Math.sin(Math.PI  / 5);
+        double alan = (5 * Math.pow(s , 2)) / (4 * Math.tan(Math.PI / 5));
+
+
+        double yuvarlanmısDeger = Math.round(alan * 100.0) / 100.0;
+        System.out.println("Alan: " + yuvarlanmısDeger);
+    }
+}
---
+++ src/chapter4/S02.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class S02 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Lütfen enlem derecesini giriniz :");
+        double enlem = scan.nextDouble();
+        System.out.print(" Lütfen boylam derecesini giriniz :");
+        double boylam = scan.nextDouble();
+
+
+
+    }
+}
